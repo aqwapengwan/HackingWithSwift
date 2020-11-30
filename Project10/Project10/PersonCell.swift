e6caf7a6806b5549894018b894a6c72c300a12cb
aqwapengwan <35090728+aqwapengwan@users.noreply.github.com>
2020-11-30 16:40:09 -0500

    Add files via upload

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Alanna Romao on 11/10/20.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Alanna Romao on 11/9/20.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
